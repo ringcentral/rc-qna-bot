d376f7cc4cf5275b3fe4e9d69c08d66cae4e1d45
Tyler Long <tyler4long@gmail.com>
2018-02-28 17:33:57 +0800

    A simple bot

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('hello world')
+const dotenv = require('dotenv')
+const striptags = require('striptags')
+
+dotenv.config()
+
+const GlipSocket = require('glip.socket.io')
+const client = new GlipSocket({
+  host: process.env.GLIP_HOST || 'app.glip.com',
+  port: process.env.GLIP_PORT || 443,
+  user: process.env.GLIP_EMAIL,
+  password: process.env.GLIP_PASSWORD
+})
+client.on('message', (type, data) => {
+  if (type !== client.type_ids.TYPE_ID_POST) {
+    return
+  }
+  if (data.text.indexOf("<a class='at_mention_compose'") === -1) {
+    return
+  }
+  if (data.text.indexOf(`>@${process.env.GLIP_NAME}</a>`) === -1) {
+    return
+  }
+  console.log(data.text)
+  const text = striptags(data.text).replace(`@${process.env.GLIP_NAME} `, '').trim()
+  console.log(text)
+  if (text === 'ping') {
+    client.post(data.group_id, 'pong')
+  }
+})
+client.start()
