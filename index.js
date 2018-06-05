e0e8e9d39c5734edfb5d355c1b13ab207c428fdb
Tyler Long <tyler4long@gmail.com>
2018-06-05 17:06:45 +0800

    First working vesion for lambda

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
-const striptags = require('striptags')
 const axios = require('axios')
 const AllHtmlEntities = require('html-entities').AllHtmlEntities
 const R = require('ramda')
@@ -12,6 +11,12 @@ const RingCentral = require('ringcentral-js-concise')
 const rc = new RingCentral('', '', process.env.RINGCENTRAL_SERVER)
 const token = JSON.parse(process.env.RINGCENTRAL_TOKEN)
 rc.token(token)
+
+const mentionRegExp = new RegExp(`\\!\\[:Person\\]\\(${token.owner_id}\\)`, 'g')
+
+const postMessage = (groupId, text) => {
+  rc.post('/restapi/v1.0/glip/posts', { groupId, text })
+}
 
 const handleMessage = (event, context, callback) => {
   if (event.headers['Verification-Token'] !== process.env.RINGCENTRAL_VERIFICATION_TOKEN) {
@@ -32,15 +37,53 @@ const handleMessage = (event, context, callback) => {
   if (R.isNil(message.text) || R.isEmpty(message.text)) {
     return
   }
-  console.log(message)
-  // rc.post('/restapi/v1.0/glip/posts', {
-  //   groupId: message.groupId,
-  //   text: JSON.stringify(message, null, 2),
-  //   attachments: undefined
-  // })
+  if (message.text.indexOf(`![:Person](${token.owner_id})`) === -1) {
+    return
+  }
+
+  const text = message.text.replace(mentionRegExp, '').trim().replace(/\s+/g, ' ')
+  if (text === 'ping') {
+    postMessage(message.groupId, 'pong')
+    return
+  }
+
+  console.log(text)
+
+  axios({
+    method: 'post',
+    url: process.env.QNA_MARKER_URI,
+    headers: {
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key': process.env.QNA_MARKER_KEY
+    },
+    data: {
+      question: text,
+      top: 2
+    }
+  }).then(r => {
+    console.log(JSON.stringify(r.data, null, 2))
+
+    const answers = R.filter(a => a.score > 20, r.data.answers)
+    if (answers.length === 0) {
+      postMessage(message.groupId, 'This question is not in my knowledge base')
+      return
+    }
+
+    let answer = 'I find the following Q & A pair(s) from my knowledge base:'
+    for (let i = 0; i < answers.length; i++) {
+      answer += `
+
+      **Q: ${answers[i].questions[0]}**
+      **A:** ${entities.decode(answers[i].answer)}`
+    }
+    postMessage(message.groupId, answer)
+  }).catch(error => {
+    console.log(error)
+  })
 }
 
-if (process.env.RINGCENTRAL_SERVER.indexOf('devtest') !== -1) { // dev
+// This is for dev only
+if (process.env.RINGCENTRAL_SERVER.indexOf('devtest') !== -1) {
   const PubNub = require('ringcentral-js-concise/src/pubnub')
   const pubnub = new PubNub(rc, ['/restapi/v1.0/glip/posts'], message => {
     handleMessage({
@@ -57,49 +100,3 @@ if (process.env.RINGCENTRAL_SERVER.indexOf('devtest') !== -1) { // dev
 }
 
 module.exports = { handleMessage }
-
-//   if (data.text.indexOf("<a class='at_mention_compose'") === -1) {
-//     return
-//   }
-//   if (data.text.indexOf(`>@${process.env.GLIP_NAME}</a>`) === -1) {
-//     return
-//   }
-//   const text = striptags(data.text).replace(`@${process.env.GLIP_NAME}`, ' ').trim().replace(/\s+/g, ' ')
-//   if (text === 'ping') {
-//     client.post(data.group_id, 'pong')
-//     return
-//   }
-//   console.log(text)
-//   axios({
-//     method: 'post',
-//     url: process.env.QNA_MARKER_URI,
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Ocp-Apim-Subscription-Key': process.env.QNA_MARKER_KEY
-//     },
-//     data: {
-//       question: text,
-//       top: 2
-//     }
-//   }).then(r => {
-//     console.log(JSON.stringify(r.data, null, 2))
-
-//     const answers = R.filter(a => a.score > 20, r.data.answers)
-//     if (answers.length === 0) {
-//       client.post(data.group_id, 'This question is not in my knowledge base')
-//       return
-//     }
-
-//     let answer = 'I find the following Q & A pair(s) from my knowledge base:'
-//     for (let i = 0; i < answers.length; i++) {
-//       answer += `
-
-//       **Q: ${answers[i].questions[0]}**
-//       **A:** ${entities.decode(answers[i].answer)}`
-//     }
-//     client.post(data.group_id, answer)
-//   }).catch(error => {
-//     console.log(error)
-//   })
-// })
-// client.start()
